1fe1a7290efb0fd8e45f3f1099fabe83da79fb03
Maxim Khvatov <maximkhvatovv@yandex.ru>
2024-09-08 22:01:51 +0500

    init commit

---
+++ src/main/java/ru/khvatov/pets/multithreading_with_vladzuev/l1/Runner.java
@@ -0,0 +1,7 @@
+package ru.khvatov.pets.multithreading_with_vladzuev.l1;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Course introduction...");
+    }
+}
